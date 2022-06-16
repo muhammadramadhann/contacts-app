07f7b8becaab8fb9ba08ee6706465a8afdebba8f
muhammadramadhann <ramadhannkurniawan@gmail.com>
2022-06-16 15:38:43 +0700

    Contact Data Input

---
+++ src/components/ContactInput.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+class ContactInput extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // inisialisasi stat
+        this.state = {
+            name: '',
+            tag: '',
+        }
+
+        this.onNameChangeEventHandler = this.onNameChangeEventHandler.bind(this);
+        this.onTagChaneEventHandler = this.onTagChaneEventHandler.bind(this);
+        this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this);
+    }
+
+    onNameChangeEventHandler(event) {
+        this.setState((prevState) => {
+            return {
+                ...prevState,
+                name: event.target.value,
+            }
+        });
+    }
+
+    onTagChaneEventHandler(event) {
+        this.setState((prevState) => {
+            return {
+                ...prevState,
+                tag: event.target.value,
+            }
+        });
+    }
+
+    onSubmitEventHandler(event) {
+
+        event.preventDefault();
+
+        // validation
+        if (!this.state.name || !this.state.tag) {
+            alert('Semua field input harus diisi!');
+            return;
+        }
+
+        // add contact
+        this.props.addContact(this.state);
+
+        // reset form input after submit
+        this.setState({
+            name: '',
+            tag: '',
+        });
+    }
+
+    render() {
+        return (
+            <form className="contact-input" onSubmit={this.onSubmitEventHandler}>
+                <input type="text" placeholder="Nama" value={this.state.name} onChange={this.onNameChangeEventHandler} />
+                <input type="text" placeholder="Tag" value={this.state.tag} onChange={this.onTagChaneEventHandler} />
+                <button type="submit">Tambah</button>
+            </form>
+        );
+    }
+}
+
+export default ContactInput;
